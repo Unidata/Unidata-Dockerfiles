aaf55420cb6798aec3495f22aeb1fd8cd9a56713
Julien Chastang <julien.c.chastang@gmail.com>
2015-04-15 12:45:17 -0600

    Parameterizing output directory

--- idv-test/test_idv.sh
+++ idv-test/test_idv.sh
@@ -2,13 +2,13 @@
 #
 # Convienence script to launch a docker container version of the IDV with all of the appropriate arguments.
 
-if [ $# -lt 1 ]; then
-    docker images
-    echo ""
-    echo "You must specify a docker image."
+
+if [ $# -ne 2 ]
+then
+    echo "Syntax: $(basename $0) <docker image> <output dir>"
     exit 1
 fi
 
 set -x
 
-docker run -v ~/temp:/home/idv/test-output -p 5901:5901 --rm -it $1 /home/idv/starttest.sh
+docker run -v $2:/home/idv/test-output -p 5901:5901 --rm -it $1 /home/idv/starttest.sh
