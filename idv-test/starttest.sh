59dd0a8e61567f448235eacee81866d153d41275
Julien Chastang <julien.c.chastang@gmail.com>
2015-04-23 14:57:16 -0600

    ensuring we grab the latest IDV

--- idv-test/starttest.sh
+++ idv-test/starttest.sh
@@ -5,6 +5,12 @@ sleep 5
 export DISPLAY=localhost:1 
 #/home/idv/IDV/testIDV ~/test-bundles/addeimage1.xidv ~/test-output/results
 
+# Kludge to ensure we are testing the nightly
+
+curl -SL https://www.unidata.ucar.edu/software/idv/nightly/webstart/IDV/idv.jar -o /home/idv/IDV/idv.jar
+curl -SL https://www.unidata.ucar.edu/software/idv/nightly/webstart/IDV/visad.jar -o /home/idv/IDV/visad.jar
+curl -SL https://www.unidata.ucar.edu/software/idv/nightly/webstart/IDV/ncIdv.jar -o /home/idv/IDV/ncIdv.jar
+
 for file in test-bundles/* ; do
     echo TESTING $file
     /home/idv/IDV/testIDV $file ~/test-output/results
