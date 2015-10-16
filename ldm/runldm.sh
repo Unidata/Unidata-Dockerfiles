2f970f37d51b50dcb9f821eae3f113ab213f83cf
Ward Fisher <wfisher@ucar.edu>
2015-10-16 11:31:30 -0600

    Further tweaking of the script used to launch LDM from doker container.

--- ldm/runldm.sh
+++ ldm/runldm.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 set -e
-
+set -x
+export PATH=/home/ldm/bin:$PATH
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+
+mkdir -p /home/ldm/var/queues/
 
 /usr/sbin/rsyslogd
 
+ldmadmin mkqueue
 ldmadmin start
