4b5560b1ed643696db8a5d7b75f4ed0230cad6f4
Julien Chastang <julien.c.chastang@gmail.com>
2016-04-25 14:35:22 -0600

    bumping docker-compose version

--- ams2016/unicloud-2.sh
+++ ams2016/unicloud-2.sh
@@ -14,7 +14,7 @@ sudo service docker restart
 
 # Get docker-compose
   curl -L \
-  https://github.com/docker/compose/releases/download/1.6.2/\
+  https://github.com/docker/compose/releases/download/1.7.0/\
 docker-compose-`uname -s`-`uname -m` > docker-compose
   sudo mv docker-compose /usr/local/bin/
   sudo chmod +x /usr/local/bin/docker-compose
