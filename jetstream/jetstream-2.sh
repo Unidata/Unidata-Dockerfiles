7c657913016f45ef399c8290863fb467e4b6ba8e
Julien Chastang <julien.c.chastang@gmail.com>
2016-12-20 18:40:35 -0700

    updating jetstream scripts

--- jetstream/jetstream-2.sh
+++ jetstream/jetstream-2.sh
@@ -8,3 +8,7 @@ bash ~/git/Unidata-Dockerfiles/jetstream/openstack/mount.sh -m /dev/sdb \
      -d /data
 bash ~/git/Unidata-Dockerfiles/jetstream/openstack/mount.sh -m /dev/sdc \
      -d /repository
+
+# ensure disks reappear on startup
+echo /dev/sdb   /data   ext4  rw      0 0 | tee --append /etc/fstab > /dev/null
+echo /dev/sdc   /repository   ext4  rw      0 0 | tee --append /etc/fstab > /dev/null
--- jetstream/jetstream-3.sh
+++ jetstream/jetstream-3.sh
@@ -10,19 +10,25 @@ mkdir -p ~/logs/ldm/ ~/logs/ramadda-tomcat/ ~/logs/ramadda/ ~/logs/tds-tomcat/ \
 mkdir -p ~/etc
 cp -r ~/git/Unidata-Dockerfiles/jetstream/etc/* ~/etc/
 
+sed -i \
+    s/server\ 0.ubuntu.pool.ntp.org/server\ timeserver.unidata.ucar.edu\\nserver\ 0.ubuntu.pool.ntp.org/g \
+    /etc/ntp.conf
+
 mkdir -p ~/tdsconfig/ ~/etc/TDS
 cp ~/git/TdsConfig/idd/config.zip ~/tdsconfig/
 unzip ~/tdsconfig/config.zip -d ~/tdsconfig/
 cp -r ~/tdsconfig/pqacts/* ~/etc/TDS
 
 # in place change of logs dir w/ sed
+
 sed -i s/logs\\/ldm-mcidas.log/var\\/logs\\/ldm-mcidas\\.log/g \
     ~/etc/TDS/util/ldmfile.sh
 
 chmod +x ~/etc/TDS/util/ldmfile.sh
 
 # Create RAMADDA default password
-echo ramadda.install.password=changeme! | sudo tee --append \
+
+echo ramadda.install.password=changeme! | tee --append \
   /repository/pw.properties > /dev/null
 
 cp ~/git/Unidata-Dockerfiles/jetstream/mcidas/pqact.conf_mcidasA ~/etc
@@ -34,7 +40,8 @@ openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj \
   -keyout ~/git/Unidata-Dockerfiles/jetstream/files/ssl.key \
   -out ~/git/Unidata-Dockerfiles/jetstream/files/ssl.crt
 
-curl -SL  https://artifacts.unidata.ucar.edu/content/repositories/unidata-releases/edu/ucar/tdmFat/4.6.6/tdmFat-4.6.6.jar \
+curl -SL  \
+     https://artifacts.unidata.ucar.edu/content/repositories/unidata-releases/edu/ucar/tdmFat/4.6.6/tdmFat-4.6.6.jar \
      -o ~/logs/tdm/tdm.jar
 curl -SL https://raw.githubusercontent.com/Unidata/thredds-docker/master/tdm/tdm.sh \
      -o ~/logs/tdm/tdm.sh
